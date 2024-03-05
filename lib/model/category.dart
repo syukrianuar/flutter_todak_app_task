d09f5236e75ca66ccbc9e175988249344975a050
Muhamad Syukri Anuar <2018638988@student.uitm.edu.my>
2024-03-05 13:44:55 +0800

    home : scrollable products index on home

---
+++ lib/model/category.dart
@@ -0,0 +1,9 @@
+
+class Category {
+  const Category(
+    this.title,
+  );
+
+  final String title;
+  // final Color color;
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todak_app_task/widget/list_items.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            'eShop',
+          ),
+        ),
+        body: const ListItems());
+  }
+}
